57bff0bb2bf4dcae0786332aca5b60409bfbd707
papadoxie <nofilqasim@gmail.com>
2022-04-08 02:05:25 +0500

    Added terminal escape sequences

---
+++ more/include/format.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef __FORMAT_H__
+#define __FORMAT_H__
+
+#define ESC_FG_RED "\033[31m"
+#define ESC_FG_GREEN "\033[32m"
+#define ESC_FG_YELLOW "\033[33m"
+#define ESC_FG_BLUE "\033[34m"
+#define ESC_FG_MAGENTA "\033[35m"
+#define ESC_FG_CYAN "\033[36m"
+#define ESC_FG_WHITE "\033[37m"
+#define ESC_FG_DEFAULT "\033[39m"
+
+#define ESC_BG_RED "\033[41m"
+#define ESC_BG_GREEN "\033[42m"
+#define ESC_BG_YELLOW "\033[43m"
+#define ESC_BG_BLUE "\033[44m"
+#define ESC_BG_MAGENTA "\033[45m"
+#define ESC_BG_CYAN "\033[46m"
+#define ESC_BG_WHITE "\033[47m"
+#define ESC_BG_DEFAULT "\033[49m"
+
+#define ESC_REVERSE "\033[7m"
+#define ESC_RESET "\033[0m"
+
+#endif // __FORMAT_H__
