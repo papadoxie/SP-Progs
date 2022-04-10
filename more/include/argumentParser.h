08369fc8392c6f08653c02ee98017497732143c4
papadoxie <nofilqasim@gmail.com>
2022-04-10 09:14:28 +0500

    Moved definitions to C file

--- more/include/argumentParser.h
+++ more/include/argumentParser.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <argp.h>
+#include <more.h> // To jump into more functionality 😁
 
 // Unused fields in argp struct
 #define ARGP_CHILDREN NULL
@@ -16,16 +17,9 @@
 error_t parse_opt(int key, char *arg, struct argp_state *state);
 
 // Variables used for argp
-const char *argp_program_version = "more 0.1.0";
-const char *argp_program_bug_address = "nofilqasim@gmail.com";
-const char doc[] = "more -- a program to display a file one screenful at a time";
-char args_doc[] = "FILE";
-struct argp_option options[] =
-    {
-        {0, 0, 0, 0, "Output options:", 1},
-        {"filename", 777, "FILENAME", 0, "The name of the file to display", 1},
-        {"version", 'v', 0, 0, "output version information", 1},
-        {0, 0, 0, 0, 0, 0}};
+extern const char doc[60];
+extern char args_doc[5];
+extern struct argp_option options[4];
 
 struct arguments
 {
