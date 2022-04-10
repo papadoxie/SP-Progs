0c068ae26ea381c9cffac3c852ae5864472cf167
papadoxie <nofilqasim@gmail.com>
2022-04-10 09:14:47 +0500

    Moved definitions to C file

--- more/source/argumentParser.c
+++ more/source/argumentParser.c
@@ -1,9 +1,19 @@
 #include <argumentParser.h>
 
+const char *argp_program_version = "more 0.1.0";
+const char *argp_program_bug_address = "nofilqasim@gmail.com";
+const char doc[] = "more -- a program to display a file one screenful at a time";
+char args_doc[] = "FILE";
+
+struct argp_option options[] =
+    {
+        {0, 0, 0, 0, "Output options:", 1},
+        {"filename", 777, "FILENAME", 0, "The name of the file to display", 1},
+        {"version", 'v', 0, 0, "output version information", 1},
+        {0, 0, 0, 0, 0, 0}};
+
 error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
-    struct arguments *arguments = state->input;
-
     switch (key)
     {
     case ARGP_KEY_ARG:
@@ -11,7 +21,8 @@ error_t parse_opt(int key, char *arg, struct argp_state *state)
         {
             argp_usage(state);
         }
-        strncpy(arguments->filename, arg, 255);
+        // Start actual program
+        more(arg);
         break;
 
     case ARGP_KEY_END:
