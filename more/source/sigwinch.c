7bd8b2b548d7e0127b474466f1311001d6cc8a34
papadoxie <nofilqasim@gmail.com>
2022-04-10 11:35:19 +0500

    Added call to buffer init in sigwinch handler

--- more/source/sigwinch.c
+++ more/source/sigwinch.c
@@ -1,23 +1,24 @@
 #include <sigwinch.h>
 
 struct window window;
+extern int initBuffer(void);
 
 void handle_sigwinch(int sig)
 {
     if (sig == SIGWINCH)
     {
-    #if defined(TIOCGSIZE)
+#if defined(TIOCGSIZE)
         struct ttysize ts;
         ioctl(STDOUT, TIOCGSIZE, &ts);
         rows = ts.ts_rows;
         cols = ts.ts_cols;
 
-    #elif defined(TIOCGWINSZ)
+#elif defined(TIOCGWINSZ)
         struct winsize ws;
         ioctl(STDOUT, TIOCGWINSZ, &ws);
         window.rows = ws.ws_row;
         window.cols = ws.ws_col;
-
-    #endif   
+        initBuffer();
+#endif
     }
 }
