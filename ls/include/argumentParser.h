0f6c47dd5e87b21dc35bf7981cd79d8688044cc9
papadoxie <nofilqasim@gmail.com>
2022-04-15 02:32:30 +0500

    Added defs for filename max len

--- ls/include/argumentParser.h
+++ ls/include/argumentParser.h
@@ -13,6 +13,8 @@
 #define HELP_FILTER NULL
 #define ARGP_DOMAIN NULL
 
+#define FILENAME_MAX 255
+
 // Functions needed for argp
 error_t parse_opt(int key, char *arg, struct argp_state *state);
 
@@ -23,7 +25,7 @@ extern struct argp_option options[10];
 
 struct arguments
 {
-    char directory[255];
+    char directory[256];
     int all;
     int long_format;
     int reverse;
