3eeda64c1a778e981df43bad7b57a2e9e67a5f56
papadoxie <nofilqasim@gmail.com>
2022-04-15 01:38:57 +0500

    Added more options

--- ls/source/argumentParser.c
+++ ls/source/argumentParser.c
@@ -9,20 +9,54 @@ struct argp_option options[] =
     {
         {0, 0, 0, 0, "Output options:", 1},
         {"directory", 777, "DIRECTORY", OPTION_ARG_OPTIONAL, "The path of the directory to list", 1},
+        {"all", 'a', 0, 0, "List all files, including hidden files", 1},
+        {"long", 'l', 0, 0, "List in long format", 1},
+        {"reverse", 'r', 0, 0, "Reverse the order of the output", 1},
+        {"sort", 's', 0, 0, "Sort the output", 1},
+        {"time", 't', 0, 0, "Sort by time", 1},
+        {"help", 'h', 0, 0, "Show this help", 1},
         {"version", 'v', 0, 0, "output version information", 1},
         {0, 0, 0, 0, 0, 0}};
 
 error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
+    struct arguments *arguments = state->input;
     switch (key)
     {
+    case 'a':
+        arguments->all = 1;
+        break;
+    
+    case 'l':
+        arguments->long_format = 1;
+        break;
+    
+    case 'r':
+        arguments->reverse = 1;
+        break;
+    
+    case 's':
+        arguments->sort = 1;
+        break;
+    
+    case 't':
+        arguments->time = 1;
+        break;
+    
+    case 'h':
+        argp_state_help(state, stdout, ARGP_HELP_LONG);
+        exit(0);
+    
+    case 'v':
+        printf("%s\n", argp_program_version);
+        exit(0);
+
     case ARGP_KEY_ARG:
         if (state->arg_num > 1)
         {
             argp_usage(state);
         }
-        // Start actual program
-        ls(arg);
+        strncpy(arguments->directory, arg, 255);
         break;
 
     case ARGP_KEY_END:
@@ -33,7 +67,7 @@ error_t parse_opt(int key, char *arg, struct argp_state *state)
             {
                 argp_usage(state);
             }
-            ls(pwd);
+            strncpy(arguments->directory, pwd, 255);
         }
         break;
     }
