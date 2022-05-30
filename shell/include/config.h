b0b26a3541691a5fa34af165aa1704bed9794939
papadoxie <nofilqasim@gmail.com>
2022-05-31 01:16:50 +0500

    Added colour to default prompt

--- shell/include/config.h
+++ shell/include/config.h
@@ -8,6 +8,12 @@
 #include <stdio.h>
 
 #include <prompt.h>
+#include <format.h>
+
+#define DEFAULT_PROMPT "" ESC_FG_RED "{username}" ESC_FG_DEFAULT                    \
+                       "@" ESC_FG_RED ESC_DIM "{hostname}" ESC_RESET ESC_FG_DEFAULT \
+                           ESC_FG_MAGENTA " {pwd}" ESC_FG_DEFAULT                   \
+                       " $ "
 
 int set_config(FILE *config_file);
 int set_default_config(void);
