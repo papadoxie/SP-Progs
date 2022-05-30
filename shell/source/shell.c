77785ed74c9d2270f80d72778558bf59a7b7399f
papadoxie <nofilqasim@gmail.com>
2022-05-31 01:16:11 +0500

    Added a jmp_buf for exit

--- shell/source/shell.c
+++ shell/source/shell.c
@@ -2,15 +2,20 @@
 
 int init_shell(void)
 {
+    // Configs will be read from these files in order
     const char *config_file_paths[] = {
         "./.cronconfig",
         "~/.config/.cronconfig",
-        "tools/.cronconfig",    // For testing
-        "../tools/.cronconfig", // For testing
+        "/etc/cronconfig",
+        "tools/.cronconfig",    //? For testing
+        "../tools/.cronconfig", //? For testing
         NULL};
 
     FILE *config_file = NULL;
     const char **path = config_file_paths;
+
+    // Using pointer so won't have to keep track of length in case 
+    // new config paths are added
     while (*path)
     {
         config_file = fopen(*path, "r");
@@ -21,40 +26,64 @@ int init_shell(void)
         path++;
     }
 
+    // If no config file found, set default config
     if (!config_file)
     {
         set_default_config();
         return 0;
     }
 
+    //TODO change this to parse config file when available
     set_default_config();
     fclose(config_file);
     return 0;
 }
 
+// Jump buffer for exit routines
+// Won't be provided in headers
+// Must be declared using extern in source file
+jmp_buf exit_buf;
+
+// Start of interactive shell
 int interactive_shell(void)
 {
+    char *command_string = NULL;
+    commands_t *commands = NULL;
+
+    // Cleanup on exit
+    if(setjmp(exit_buf))
+    {
+        free_commands(commands ? commands : NULL);
+        free(command_string ? command_string : NULL);
+        return 0;
+    }
+
     while (true)
     {
+        // Display prompt
         print_prompt();
-
-        char *command_string = read_commandline(stdin);
+        
+        // Read command line from stdin
+        command_string = read_commandline(stdin);
         if (!command_string)
         {
             continue;
         }
-
-        commands_t *commands = parse_commandline(command_string);
+        
+        // Parse command line into command structs
+        commands = parse_commandline(command_string);
         if (!commands)
         {
             continue;
         }
-
+        
+        // Execute commands in commands struct
         execute(commands);
 
-        free_commands(commands);
+        // Cleanup on each iteration
+        free_commands(commands ? commands : NULL);
         commands = NULL;
-        free(command_string);
+        free(command_string ? command_string : NULL);
         command_string = NULL;
     }
     return 0;
