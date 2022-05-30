d825db3893d5b324eb285fd73bbe42d393c8a445
papadoxie <nofilqasim@gmail.com>
2022-05-31 01:15:14 +0500

    Added comments

--- shell/source/main.c
+++ shell/source/main.c
@@ -1,10 +1,17 @@
 #include <locale.h>
 #include <shell.h>
 
+// Entry point
 int main(void) 
 {
+    // Enable support for wchar_t
     setlocale(LC_ALL, "");
+    
+    // Initialize the shell environment and configuration
     init_shell();
+
+    // Start an interactive shell
+    // Will add support for scripting later
     interactive_shell();
     return 0;
 }
