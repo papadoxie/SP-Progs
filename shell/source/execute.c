b5815376747c4073869c0231f6f46cf42ff696f9
papadoxie <nofilqasim@gmail.com>
2022-05-30 20:39:43 +0500

    Changed exit() to _Exit() for child

--- shell/source/execute.c
+++ shell/source/execute.c
@@ -3,7 +3,13 @@
 int execute(commands_t *commands)
 {
     process_t *processes = malloc(sizeof(process_t) * commands->num_commands);
+    if (!processes)
+    {
+        perror("Could not run processes");
+        return -1;
+    }
 
+    int final_status = 0;
     for (uint32_t i = 0; i < commands->num_commands; i++)
     {
         process_t cur_proc;
@@ -17,12 +23,13 @@ int execute(commands_t *commands)
             if (cur_proc.status == -1)
             {
                 printf("cronShell: Unknown command: %s\n", cur_proc.command->command);
-                exit(EXIT_FAILURE);
+                _Exit(EXIT_FAILURE);
             }
         }
         waitpid(cur_proc.pid, &cur_proc.status, 0);
+        final_status = WEXITSTATUS(cur_proc.status);
     }
 
     free(processes);
-    return 0;
+    return final_status;
 }
