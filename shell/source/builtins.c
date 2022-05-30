984191a96b72c293473860ce29a35dd969b64ac8
papadoxie <nofilqasim@gmail.com>
2022-05-31 02:06:15 +0500

    Fixed freeing loop according to argc

--- shell/source/builtins.c
+++ shell/source/builtins.c
@@ -72,7 +72,7 @@ int pwd(int argc,
 // Print command line arguments
 int echo(int argc, char **argv)
 {
-    for (int i = 1; i <= argc; i++)
+    for (int i = 1; i < argc; i++)
     {
         printf("%s ", argv[i]);
     }
